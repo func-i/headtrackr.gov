ac472ee6ee6bc5da160ce3b0de6afa566a3824cc
Alex Naser <c.a.naser@gmail.com>
2013-12-13 16:37:07 -0500

    customizing messages

--- javascripts/headtrackr.js
+++ javascripts/headtrackr.js
@@ -1590,22 +1590,22 @@ headtrackr.Ui = function() {
         p = document.createElement('p');
   d.setAttribute('id', 'headtrackerMessageDiv');
   
-  d.style.left = "20%";
-  d.style.right = "20%";
+  d.style.left = "30%";
+  d.style.right = "30%";
   d.style.top = "30%";
-  d.style.fontSize = "90px";
+  d.style.fontSize = "20px";
   d.style.color = "#777";
   d.style.position = "absolute";
-  d.style.fontFamily = "Helvetica, Arial, sans-serif";
+  d.style.fontFamily = "Courier";
   d.style.zIndex = '100002';
   
   d2.style.marginLeft = "auto";
   d2.style.marginRight = "auto";
   d2.style.width = "100%";
   d2.style.textAlign = "center";
-  d2.style.color = "#fff";
+  d2.style.color = "#00FF00";
   d2.style.backgroundColor = "#444";
-  d2.style.opacity = "0.5";
+  d2.style.opacity = "0.99";
   
   p.setAttribute('id', 'headtrackerMessage');
   d2.appendChild(p);
@@ -1618,12 +1618,12 @@ headtrackr.Ui = function() {
   };
   
   var statusMessages = {
-    "whitebalance" : "Waiting for camera whitebalancing",
-    "detecting" : "Please wait while camera is detecting your face...",
-    "hints" : "We seem to have some problems detecting your face. Please make sure that your face is well and evenly lighted, and that your camera is working.",
+    "whitebalance" : "Please stay still while we... um... scan your face.",
+    "detecting" : "Please stay still while we... um... scan your face.",
+    "hints" : "We had some problems. Please make sure that your face is well and evenly lighted, and that your camera is working.",
     "redetecting" : "Lost track of face, trying to detect again..",
     "lost" : "Lost track of face :(",
-    "found" : "Face found! Move your head!"
+    "found" : "Face scanned! But you have nothing to worry about!"
   };
   
   var override = false;
